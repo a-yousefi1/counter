bb3424b99230c072a712a6b4de2392a5b2807901
a-yousefi1 <a.yousefi4784@gmail.com>
2023-11-28 22:46:21 +0330

    number color function added

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+function number_color(number){
+    if (number < 0){
+        console.log("test");
+        document.querySelector("p").style.color = "red"
+    }
+    else if (number > 0){
+        document.querySelector("p").style.color = "green"
+    }
+    else {
+        document.querySelector("p").style.color = "white"
+    }
+}
 
 function decrease_number(){
     let number = Number(document.querySelector("p").textContent)
     number -= 1
     document.querySelector("p").innerHTML = number
+    number_color(number)
 }
